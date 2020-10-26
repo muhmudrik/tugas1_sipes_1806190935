b86a67f306d9aa919368b43dc5afa88c068a3720
ruka <rokamudrik@gmail.com>
2020-10-26 18:04:37 +0700

    feat: no 1 dan navbar

--- sipes/src/main/java/apap/tugas/sipes/controller/PesawatController.java
+++ sipes/src/main/java/apap/tugas/sipes/controller/PesawatController.java
@@ -1,8 +1,28 @@
 package apap.tugas.sipes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import apap.tugas.sipes.service.PesawatService;
 
 @Controller
 public class PesawatController {
+    @Autowired
+    private PesawatService pesawatService;
+
+    @GetMapping("/")
+    private String home() {
+        return "home";
+    }
     
+    
+
+    @GetMapping("/pesawat")
+    private String getAllPesawat(Model model){
+
+        return "view-all-pesawat";
+    }
+
 }
