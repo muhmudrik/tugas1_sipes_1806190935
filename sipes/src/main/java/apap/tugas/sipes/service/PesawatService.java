fdf5b3c06271fe2a6c65d8a0d14d7fda0ef8b0ed
ruka <rokamudrik@gmail.com>
2020-10-28 19:50:04 +0700

    fix: changed how db save jens_pesawt

--- sipes/src/main/java/apap/tugas/sipes/service/PesawatService.java
+++ sipes/src/main/java/apap/tugas/sipes/service/PesawatService.java
@@ -1,9 +1,12 @@
 package apap.tugas.sipes.service;
 
+import java.util.List;
+
 import apap.tugas.sipes.model.PesawatModel;
 
 public interface PesawatService {
 
 	void addPesawat(PesawatModel pesawat);
    
+	List<PesawatModel> getAllPesawat();
 }
